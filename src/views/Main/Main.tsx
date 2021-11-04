1907b70aabfbb80f5b9bc7e2807747311efa819a
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-04 08:48:10 -0300

    Add Main View

---
+++ src/views/Main/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Header, MainContent } from '../../components'
+import './main.css'
+
+
+const Main = () => {
+  return (
+    <div className='main'>
+     <Header />
+     <MainContent />
+    </div>
+  )
+}
+
+export default Main

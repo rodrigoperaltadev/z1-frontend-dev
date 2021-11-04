3d2ff03b52477e24b30471fdc2b75a3fea4d359f
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-04 08:46:06 -0300

    Add header

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Logo, Divider } from '../'
+import './header.css'
+
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <Logo />
+      <Divider />
+    </header>
+  )
+}
+
+export default Header

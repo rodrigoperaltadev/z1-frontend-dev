0025cdee4535d7703371e829815262c1b7a7cfbd
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-04 08:46:34 -0300

    Add component

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+export { default as Header } from './Header'
+export { default as MainContent } from './MainContent'
+export { default as Logo } from './Logo'
+export { default as Divider } from './Divider'
+export { default as EmptyCard } from './EmptyCard'
+export { default as EmptyPhoto } from './EmptyPhoto'

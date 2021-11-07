5a04376d2318040616a5df670efa09deed3177f3
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-07 12:08:43 -0300

    Add icons

--- src/views/TakePicture/TakePicture.tsx
+++ src/views/TakePicture/TakePicture.tsx
@@ -1,6 +1,7 @@
 import React, { FC } from 'react'
 import { useInterval } from './../../hooks'
 import { Camera } from '../../components'
+import { ReactComponent as Bulb } from './../../assets/icons/bulb.svg'
 
 import './takePicture.css'
 
@@ -17,26 +18,32 @@ const TakePicture:FC<TakePictureProps> = ({ returnToHome, badPhoto, hasPhoto, cr
 
   useInterval(() => {
     (!hasPhoto || (hasPhoto && badPhoto)) && createId()
-    console.log(hasPhoto, badPhoto)
   }, THREE_SECONDS )
 
   const MessageStatusCamera = () => {
     const message = !hasPhoto ? 'Room lighting is too low' : !badPhoto ? 'Picture taken' : ''
     return (
-      <div className='tp-text'>{message}</div>
+      <div className='tp-text'>
+        {!hasPhoto && <Bulb className='icon bulb' /> } 
+        { message }
+      </div>
     )
   }
 
   return (
-    <div className='tp-container'>
-      <h1 className='tp-title'>Take Picture</h1>
-      <div className='tp-text-wrapper'>
-        <span className='tp-text'>Fit your ID card inside the frame.</span>
-        <span className='tp-text'>The picture will be taken automactically.</span>
+    <div className='tp-view'>
+      <div className='tp-container'>
+        <header>
+          <h1 className='tp-title'>Take Picture</h1>
+          <div className='tp-text-wrapper'>
+            <span className='tp-text'>Fit your ID card inside the frame.</span>
+            <span className='tp-text'>The picture will be taken automactically.</span>
+          </div>
+        </header>
+        <Camera badPhoto={badPhoto} hasPhoto={hasPhoto} />
+        <MessageStatusCamera />
+        <button className='tp-title cancel-button' onClick={returnToHome}>CANCEL</button>
       </div>
-      <Camera badPhoto={badPhoto} hasPhoto={hasPhoto} />
-      <MessageStatusCamera />
-      <button className='tp-title cancel-button' onClick={returnToHome}>CANCEL</button>
     </div>
   )
 }

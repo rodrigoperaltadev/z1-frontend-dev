c517c7e87e318504ce3e05c1ebe490e62e096ebb
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-07 12:06:51 -0300

    Implements icons

--- src/components/PhotoStatus/PhotoStatus.tsx
+++ src/components/PhotoStatus/PhotoStatus.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react'
 
 import idImg from './../../assets/img/main-id.png'
+import { ReactComponent as Check } from './../../assets/icons/check.svg'
+import { ReactComponent as Xmark } from './../../assets/icons/xmark.svg'
 import './photoStatus.css'
 
 type PhotoStatusProps = {
@@ -13,11 +15,15 @@ const PhotoStatus: FC<PhotoStatusProps> = ({ badPhoto, onTakePictureButtonClick 
   const borderColor = badPhoto ? 'border-error' : 'border-success'
   const buttonColor = badPhoto ? 'button-error' : 'button-success'
   const message = badPhoto ? 'REJECTED' : 'ACCEPTED'
+  const icon = badPhoto ? <Xmark className='icon' /> : <Check className='icon'/>
 
   return (
     <div className='photo-container'>
       <img className={`border ${borderColor}`} src={idImg} alt="" />
-      <span className={`status-button float-button ${buttonColor}`} >{message}</span>
+      <span className={`status-button float-button ${buttonColor}`} >
+        {icon}
+        {message}
+      </span>
       {
         badPhoto && 
         <button className='button button-primary' onClick={onTakePictureButtonClick}>RETAKE PICTURE</button>

626f2a4693bcd302d477b5193801de24691d4d57
Rodrigo Peralta <49768802+rodrigoperaltadev@users.noreply.github.com>
2021-11-06 22:35:48 -0300

    Implement state with hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
-import React from 'react'
-import { Main } from './views'
+import React, { useState } from 'react'
+import api from './api'
+import { OUTCOMES } from './constants'
+import { AppProvider } from './context'
+import { Home, TakePicture } from './views'
 
-function App() {
-  return (
-    <Main />
-  );
+const VIEWS = {
+  HOME: "HOME",
+  TAKE_PICTURE: "TAKE_PICTURE"
+}
+
+const App = () => {
+
+  const [badPhoto, setBadPhoto] = useState(false)
+  const [hasPhoto, setHasPhoto] = useState(false)   
+  
+  const createId = async () => {
+    const { summary } = await api.id.createIDPayload()
+    console.log(summary)
+    setHasPhoto(true)
+    setBadPhoto(summary.outcome !== OUTCOMES.APPROVE)
+   }
+
+  const [currentView, setCurrentView] = useState(VIEWS.HOME)
+  const handleTakePictureButtonClick = () => setCurrentView(VIEWS.TAKE_PICTURE)
+  const handleReturnToHome = () => setCurrentView(VIEWS.HOME)
+
+
+  const content = currentView === VIEWS.HOME 
+    ? <Home
+        badPhoto={badPhoto}
+        hasPhoto={hasPhoto} 
+        onTakePictureButtonClick={handleTakePictureButtonClick} 
+      />
+    : <TakePicture
+        badPhoto={badPhoto}
+        hasPhoto={hasPhoto}  
+        returnToHome={handleReturnToHome}
+        createId={createId}
+      />
+
+  return (<AppProvider>{content}</AppProvider>);
 }
 
 export default App
--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -1,13 +1,23 @@
-import React from "react"
+import React, { FC } from "react"
 import { EmptyPhoto } from '../'
+import { PhotoStatus } from "../PhotoStatus"
 import './main.css'
 
-const MainContent = () => {
+type MainContentProps = {
+  onTakePictureButtonClick: () => void
+  hasPhoto: boolean
+  badPhoto: boolean
+}
+
+const MainContent:FC<MainContentProps> = ({ badPhoto, hasPhoto, onTakePictureButtonClick}) => {
+
+  const Photo = () => hasPhoto ? (<PhotoStatus badPhoto={badPhoto} onTakePictureButtonClick={onTakePictureButtonClick} />) : (<EmptyPhoto onTakePictureButtonClick={onTakePictureButtonClick} />)
+
   return (
     <div className="wrapper">
       <h1 className="title">Scan your ID</h1>
       <span className="text">Take a picture. it may take time to validate your personal information</span>
-      <EmptyPhoto />
+      <Photo />
     </div>
   )
 }
